ee40706d14dade2f198c94ed062bb53888d98a42
Panachai <panachai.ny@gmail.com>
2025-07-26 22:16:25 +0700

    refactor(main): remove Future from main function signature

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
+main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
