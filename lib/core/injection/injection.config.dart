9377c2144fdb5cb069a8ddeb6811cf4e26b81c38
Panachai <panachai.ny@gmail.com>
2025-07-28 19:17:23 +0700

    feat(network): add connectivity_plus for network status management

--- lib/core/injection/injection.config.dart
+++ lib/core/injection/injection.config.dart
@@ -9,11 +9,19 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:connectivity_plus/connectivity_plus.dart' as _i895;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
 import '../../config/app_config.dart' as _i635;
 import '../../config/environment_config.dart' as _i1070;
+import '../../features/home/data/datasources/transaction_data_source.dart'
+    as _i613;
+import '../../features/home/data/repositories/transaction_repository_impl.dart'
+    as _i1044;
+import '../../features/home/domain/repositories/transaction_repository.dart'
+    as _i705;
+import '../network/network.dart' as _i855;
 
 const String _test = 'test';
 const String _dev = 'dev';
@@ -27,6 +35,18 @@ extension GetItInjectableX on _i174.GetIt {
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     gh.singleton<_i1070.EnvironmentConfig>(() => _i1070.EnvironmentConfig());
+    gh.factory<_i613.TransactionDataSource>(
+      () => _i613.TransactionDataSourceImpl(),
+    );
+    gh.factory<_i855.NetworkInfo>(
+      () => _i855.NetworkInfoImpl(gh<_i895.Connectivity>()),
+    );
+    gh.factory<_i705.TransactionRepository>(
+      () => _i1044.TransactionRepositoryImpl(
+        dataSource: gh<_i613.TransactionDataSource>(),
+        networkInfo: gh<_i855.NetworkInfo>(),
+      ),
+    );
     gh.factory<_i635.AppConfig>(() => _i635.TestConfig(), registerFor: {_test});
     gh.factory<_i635.AppConfig>(() => _i635.DevConfig(), registerFor: {_dev});
     gh.factory<_i635.AppConfig>(() => _i635.ProdConfig(), registerFor: {_prod});
