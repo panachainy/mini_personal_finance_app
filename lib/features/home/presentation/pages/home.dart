70a7d664258c55e537df966ac3474fa21b9a95fc
Panachai <panachai.ny@gmail.com>
2025-07-28 19:45:17 +0700

    feat(home): add refresh functionality to transaction list

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_personal_finance_app/core/widgets/simpleAlert.dart';
-import 'package:mini_personal_finance_app/features/home/data/models/transaction_category_model.dart';
-import 'package:mini_personal_finance_app/features/home/data/models/transaction_model.dart';
 import 'package:mini_personal_finance_app/features/home/presentation/bloc/transaction_bloc.dart';
 
 import 'package:mini_personal_finance_app/features/home/presentation/widgets/transaction_dialog.dart';
@@ -49,47 +47,54 @@ class HomeView extends StatelessWidget {
                     ),
                     loading: () =>
                         const Center(child: CircularProgressIndicator()),
-                    loaded: (transactions) => TransactionList(
-                      transactions: state.maybeWhen(
-                        loaded: (transactions) => transactions,
-                        orElse: () => [],
+                    loaded: (transactions) => RefreshIndicator(
+                      onRefresh: () async {
+                        context.read<TransactionBloc>().add(
+                          const TransactionEvent.started(),
+                        );
+                      },
+                      child: TransactionList(
+                        transactions: state.maybeWhen(
+                          loaded: (transactions) => transactions,
+                          orElse: () => [],
+                        ),
+                        onTap: (transaction) {
+                          showDialog(
+                            context: context,
+                            builder: (context) => TransactionDialog(
+                              onSubmit:
+                                  (
+                                    String? id,
+                                    bool isExpense,
+                                    String category,
+                                    double amount,
+                                    String description,
+                                  ) {
+                                    print(
+                                      'Transaction created: $description, $amount',
+                                    );
+                                  },
+                              id: transaction.id,
+                              isExpense: transaction.category.isExpense,
+                              category: transaction.category.name,
+                              amount: transaction.amount,
+                              description: transaction.description,
+                            ),
+                          );
+                        },
+                        onDelete: (transaction) {
+                          SimpleAlert.showConfirm(
+                            context,
+                            title: 'Delete Transaction',
+                            message:
+                                'Are you sure you want to delete this transaction?',
+                            onConfirm: () {
+                              // TODO: Handle transaction deletion
+                              print('Transaction deleted: ${transaction.id}');
+                            },
+                          );
+                        },
                       ),
-                      onTap: (transaction) {
-                        showDialog(
-                          context: context,
-                          builder: (context) => TransactionDialog(
-                            onSubmit:
-                                (
-                                  String? id,
-                                  bool isExpense,
-                                  String category,
-                                  double amount,
-                                  String description,
-                                ) {
-                                  print(
-                                    'Transaction created: $description, $amount',
-                                  );
-                                },
-                            id: transaction.id,
-                            isExpense: transaction.category.isExpense,
-                            category: transaction.category.name,
-                            amount: transaction.amount,
-                            description: transaction.description,
-                          ),
-                        );
-                      },
-                      onDelete: (transaction) {
-                        SimpleAlert.showConfirm(
-                          context,
-                          title: 'Delete Transaction',
-                          message:
-                              'Are you sure you want to delete this transaction?',
-                          onConfirm: () {
-                            // TODO: Handle transaction deletion
-                            print('Transaction deleted: ${transaction.id}');
-                          },
-                        );
-                      },
                     ),
                     error: (message) => Center(child: Text('Error: $message')),
                   );
